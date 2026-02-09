67f4ded102c81004a3eb1509a0316913a1872ad7
SomozaR <20245482@esen.edu.sv>
2026-02-09 01:33:11 -0600

    Web2_Guia03

---
+++ src/hooks/useFetchMovieDetails.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { API_KEY } from "./useFetchMovies";
+
+export function useFetchMovieDetails(selectedId) {
+  const [movie, setMovie] = useState({});
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (!selectedId) {
+      setMovie({});
+      setError("");
+      return;
+    }
+
+    async function fetchMovieDetails(selectedId) {
+      try {
+        setIsLoading(true);
+        setError(null);
+
+        const response = await fetch(
+          `http://www.omdbapi.com/?apikey=${API_KEY}&i=${selectedId}`
+        );
+
+        if (!response.ok) {
+          throw new Error("Error al cargar los detalles de la película");
+        }
+
+        const data = await response.json();
+
+        setMovie(data);
+      } catch (err) {
+        setError(err.message);
+        setMovie({});
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchMovieDetails(selectedId);
+  }, [selectedId]);
+
+  return { movie, isLoading, error };
+}
